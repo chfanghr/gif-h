3d431f7db432ade4b32728fa68f69a18bfec0cb5
Hongrui Fang <chfanghr@gmail.com>
2021-04-30 05:36:44 +0800

    aplit everything to satisfy my stupid linker

---
+++ helper.cc
@@ -0,0 +1,22 @@
+//
+// Created by 方泓睿 on 2021/4/30.
+//
+
+#include "helper.h"
+
+int gif::fputs(const char *string, std::stringbuf &buffer) {
+  auto stringLength = (std::streamsize) string;
+  buffer.sputn(string, stringLength);
+  return 1;
+}
+
+int gif::fwrite(const void *ptr, size_t size, size_t count, std::stringbuf &buffer) {
+  auto totalSize = (std::streamsize) size * count;
+  buffer.sputn(static_cast<const char *>(ptr), totalSize);
+  return 1;
+}
+
+int gif::fputc(int ch, std::stringbuf &buffer) {
+  buffer.sputc((char) ch);
+  return 1;
+}
--- helper.h
+++ helper.h
@@ -2,19 +2,10 @@
 
 #include <sstream>
 
-int fputs(const char *string, std::stringbuf &buffer) {
-  auto stringLength = (std::streamsize) string;
-  buffer.sputn(string, stringLength);
-  return 1;
-}
+namespace gif {
+int fputs(const char *string, std::stringbuf &buffer);
 
-int fputc(int ch, std::stringbuf &buffer) {
-  buffer.sputc((char) ch);
-  return 1;
-}
+int fputc(int ch, std::stringbuf &buffer);
 
-int fwrite(const void *ptr, size_t size, size_t count, std::stringbuf &buffer) {
-  auto totalSize = (std::streamsize) size * count;
-  buffer.sputn(static_cast<const char *>(ptr), totalSize);
-  return 1;
+int fwrite(const void *ptr, size_t size, size_t count, std::stringbuf &buffer);
 }
